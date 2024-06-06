7290aef6494c3a30c3a65d5243e72f0ec4e56f3d
Matheus Vieira <matheus.fvp@outlook.com>
2024-06-06 10:45:05 -0300

    express configuration

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
-import express, {Request, Response} from "express";
+import express, {Request, Response, json} from "express";
 
 const app = express();
+const port = process.env.SERVER_PORT;
+
+app.use(json());
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!');
+    res.status(200).json({ player: "Vinicius Junior" });
 });
 
-app.listen(3333);
+app.listen(port, () => {
+    console.log(`🔥Server running at port ${port}`);
+});
