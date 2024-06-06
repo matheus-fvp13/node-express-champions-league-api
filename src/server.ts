30f48c13f2768c85a9701ccdc21f58130c21d2b3
Matheus Vieira <matheus.fvp@outlook.com>
2024-06-06 10:33:10 -0300

    add a server with express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-console.log("Hello World");
+import express, {Request, Response} from "express";
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!');
+});
+
+app.listen(3333);
