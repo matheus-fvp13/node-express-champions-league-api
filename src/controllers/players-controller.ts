1f9724675fa8c541ae0a5d08393f4f15aded01ea
Matheus Vieira <matheus.fvp@outlook.com>
2024-06-07 11:45:20 -0300

    update player service

--- src/controllers/players-controller.ts
+++ src/controllers/players-controller.ts
@@ -1,9 +1,7 @@
 import { Request, Response } from "express";
 import { getPlayerService } from "../services/players-service";
-import { ok } from "../utils/http-helper";
 
 export const getPlayer = async (req: Request, res: Response) => {
-    const responseService = await getPlayerService();
-    const response = await ok(responseService);
-    res.status(response.statusCode).json(response.body);
+    const httpResponse = await getPlayerService();
+    res.status(httpResponse.statusCode).json(httpResponse.body);
 }
