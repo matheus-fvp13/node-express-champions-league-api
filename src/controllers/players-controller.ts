0835fb3e5bf1f58265a02043f0625c0227e4fd3f
Matheus Vieira <matheus.fvp@outlook.com>
2024-06-06 21:20:08 -0300

    feat: http-helper

--- src/controllers/players-controller.ts
+++ src/controllers/players-controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
 import { getPlayerService } from "../services/players-service";
+import { ok } from "../utils/http-helper";
 
 export const getPlayer = async (req: Request, res: Response) => {
     const responseService = await getPlayerService();
-    res.status(200).json(responseService);
+    const response = await ok(responseService);
+    res.status(response.statusCode).json(response.body);
 }
