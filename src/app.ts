6c23fd5c6a3b74779b2a5571c013c3583931f73c
Matheus Vieira <matheus.fvp@outlook.com>
2024-06-13 13:38:31 -0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import express, { json } from "express";
 import router from "./routes";
+import cors from "cors";
 
 function createApp() {
     const app = express();
+    const corsOptions = {
+        origin: ["http://myapi.com.br"],
+        methods: [ "GET" ]
+    };
 
     app.use(json());
     app.use("/api", router);
+    app.use(cors(corsOptions));
 
     return app;
 }
--- src/repositories/clubs-repository.ts
+++ src/repositories/clubs-repository.ts
@@ -1,12 +1,6 @@
 import { ClubModel } from "../models/club-model";
 import fs from "fs/promises"
 
-const database: ClubModel[] = [
-    { id: 1, name: 'Club A' },
-    { id: 2, name: 'Club B' },
-    { id: 3, name: 'Club C' }
-];
-
 export const findAllClubs = async (): Promise<ClubModel[]> => {
     const data = await fs.readFile("./src/data/clubs.json", "utf-8");
     const clubs: ClubModel[] = JSON.parse(data);
